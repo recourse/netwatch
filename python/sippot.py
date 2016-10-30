423d61795cc933789c99a59f15504e014914011a
recourse <recourse@gmail.com>
2016-10-30 09:51:29 -0500

    sippot
    sip honeypot

--- python/sippot.py
+++ python/sippot.py
@@ -12,14 +12,7 @@ addr = (host,port)
 UDPSock = socket(AF_INET,SOCK_DGRAM)
 UDPSock.bind(addr)
 
-# Receive messages
-while 1:
-    data,addr = UDPSock.recvfrom(buf)
-    if not data:
-        print " SIP/2.0 200 OK Via: SIP/2.0/UDP there.com:5060 From: scanner <sip:suck@me.com> To: scanned <sip:eat@me.com> Call-ID: 6969@yourmomms.com CSeq: 2 SUCKIT"
-        break
-    else:
-        print "\nReceived message '", data,"' from ", addr
+
 
 # Close socket
 UDPSock.close()
